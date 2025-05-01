b2d730baca9f35d68d6e5d90eb9fcfdbda3e777f
Atstincer <atstincer87@gmail.com>
2025-05-01 19:31:32 +0200

    unicafe step 3

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -2,6 +2,23 @@ import { useState } from 'react'
 
 const Button = ({ onClick, text}) => <button onClick= {onClick}>{text}</button>
 
+const Statistics = ({good, neutral, bad}) => {
+  const getTotal = () => good + neutral + bad
+  const getAverage = () => (good*1+neutral*0+bad*-1)/getTotal()
+  const getPositivePercentage = () => good/getTotal()*100
+  return (
+    <div>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>total {getTotal()}</p>
+        <p>average {getAverage()}</p>
+        <p>positive {getPositivePercentage()} %</p>
+      </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -12,25 +29,13 @@ const App = () => {
   const handleNeutralClick = () => setNeutral(neutral + 1)
   const handleBadClick = () => setBad(bad + 1)
 
-  const getTotal = () => good + neutral + bad
-  const getAverage = () => (good*1+neutral*0+bad*-1)/getTotal()
-  const getPositivePercentage = () => good/getTotal()*100
-
   return (
     <div>
       <h1>give feedback</h1>
       <Button onClick={handleGoodClick} text="good"/>
       <Button onClick={handleNeutralClick} text="neutral"/>
       <Button onClick={handleBadClick} text="bad"/>
-      <div>
-        <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>total {getTotal()}</p>
-        <p>average {getAverage()}</p>
-        <p>positive {getPositivePercentage()} %</p>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
