b99389245e019b81e415f5182e6874c9b360e5df
Atstincer <atstincer87@gmail.com>
2025-05-28 16:33:44 +0200

    5.7 Blog List Frontend

--- Part5/bloglist-frontend/src/components/Blog.jsx
+++ Part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleVisibility = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const blogStyle = {
+    paddingTop: 7,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const blogDetails = () => {
+    return (
+      <div>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button>like</button></div>
+        <div>{blog.user.name}</div>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility}>{showDetails ? 'hide' : 'show'}</button>
+      {showDetails && blogDetails()}
+    </div>
+  )
+}
 
 export default Blog
