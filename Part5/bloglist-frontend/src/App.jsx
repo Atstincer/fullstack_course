ea4f0c9503a3d66a6de23f61a0c6bdf3eaebfa02
Atstincer <atstincer87@gmail.com>
2025-07-12 17:12:53 +0200

    7.9 getting Prettier to work

--- Part5/bloglist-frontend/src/App.jsx
+++ Part5/bloglist-frontend/src/App.jsx
@@ -1,117 +1,129 @@
-import { useState, useEffect, useRef } from 'react'
-import Blog from './components/Blog'
-import NewBlogForm from './components/NewBlogForm'
-import LoginForm from './components/LoginForm'
-import ToggleViews from './components/ToggleViews'
-import blogService from './services/blogs'
-import loginService from './services/login'
+import { useState, useEffect, useRef } from "react";
+import Blog from "./components/Blog";
+import NewBlogForm from "./components/NewBlogForm";
+import LoginForm from "./components/LoginForm";
+import ToggleViews from "./components/ToggleViews";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const Message = ({ msg }) => {
-  if (!msg) return null
-  return (
-    <div className='message'>{msg}</div>
-  )
-}
+  if (!msg) return null;
+  return <div className="message">{msg}</div>;
+};
 
 const ErrorMessage = ({ msg }) => {
-  if (!msg) return null
+  if (!msg) return null;
   return (
-    <div className='error_message' data-testid='errormsg_div'>{msg}</div>
-  )
-}
+    <div className="error_message" data-testid="errormsg_div">
+      {msg}
+    </div>
+  );
+};
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [user, setUser] = useState(null)
-  const [message, setMessage] = useState(null)
-  const [errorMessage, setErrorMessage] = useState(null)
-  const blogFormRef = useRef()
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const blogFormRef = useRef();
 
   useEffect(() => {
     async function getBlogs() {
-      const blogs = await blogService.getAll()
-      setBlogs(blogs)
+      const blogs = await blogService.getAll();
+      setBlogs(blogs);
     }
-    getBlogs()
-  }, [])
+    getBlogs();
+  }, []);
 
   useEffect(() => {
-    const userLoggedIn = window.localStorage.getItem('loggedBlogAppUser')
+    const userLoggedIn = window.localStorage.getItem("loggedBlogAppUser");
     if (userLoggedIn) {
-      const userObject = JSON.parse(userLoggedIn)
-      setUser(userObject)
-      blogService.setToken(userObject.token)
+      const userObject = JSON.parse(userLoggedIn);
+      setUser(userObject);
+      blogService.setToken(userObject.token);
     }
-  }, [])
+  }, []);
 
   const updateBlogsToShow = async () => {
-    const blogsInDb = await blogService.getAll()
-    setBlogs(blogsInDb)
-  }
+    const blogsInDb = await blogService.getAll();
+    setBlogs(blogsInDb);
+  };
 
-  const login = async credentials => {
+  const login = async (credentials) => {
     try {
-      const user = await loginService.login(credentials)
-      setUser(user)
-      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
-      blogService.setToken(user.token)
+      const user = await loginService.login(credentials);
+      setUser(user);
+      window.localStorage.setItem("loggedBlogAppUser", JSON.stringify(user));
+      blogService.setToken(user.token);
     } catch (error) {
-      setErrorMessage(error.response.data.error)
-      setTimeout(() => { setErrorMessage(null) }, 3000)
+      setErrorMessage(error.response.data.error);
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 3000);
     }
-  }
+  };
 
   const handleLogout = () => {
-    window.localStorage.removeItem('loggedBlogAppUser')
-    setUser(null)
-    blogService.setToken(null)
-  }
+    window.localStorage.removeItem("loggedBlogAppUser");
+    setUser(null);
+    blogService.setToken(null);
+  };
 
   const addNewBlog = async (newBlog) => {
     try {
       //blogFormRef.current.toggleVisibility() -> error: TypeError: blogFormRef.current.toggleVisibility is not a function
-      blogFormRef.current()
-      const blogCreated = await blogService.create(newBlog)
-      await updateBlogsToShow()
-      setMessage(`a new blog ${blogCreated.title} by ${blogCreated.author}`)
-      setTimeout(() => { setMessage(null) }, 3000)
+      blogFormRef.current();
+      const blogCreated = await blogService.create(newBlog);
+      await updateBlogsToShow();
+      setMessage(`a new blog ${blogCreated.title} by ${blogCreated.author}`);
+      setTimeout(() => {
+        setMessage(null);
+      }, 3000);
     } catch (error) {
-      console.log('error:', error)
-      setErrorMessage(error.response.data.error)
-      setTimeout(() => { setErrorMessage(null) }, 3000)
+      console.log("error:", error);
+      setErrorMessage(error.response.data.error);
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 3000);
     }
-  }
+  };
 
-  const addOneLike = async blog => {
+  const addOneLike = async (blog) => {
     try {
       //console.log('blog recibido:', blog)
-      const blogToUpdate = { ...blog, likes: blog.likes + 1 }
+      const blogToUpdate = { ...blog, likes: blog.likes + 1 };
       //console.log('blogToUpdate:', blogToUpdate)
-      const updatedBlog = await blogService.update(blogToUpdate)
-      await updateBlogsToShow()
-      setMessage(`${updatedBlog.title} has now ${updatedBlog.likes} likes`)
-      setTimeout(() => { setMessage('') }, 3000)
+      const updatedBlog = await blogService.update(blogToUpdate);
+      await updateBlogsToShow();
+      setMessage(`${updatedBlog.title} has now ${updatedBlog.likes} likes`);
+      setTimeout(() => {
+        setMessage("");
+      }, 3000);
     } catch (error) {
-      console.log('error:', error)
-      setErrorMessage(error.response.data.error)
-      setTimeout(() => { setErrorMessage('') }, 3000)
+      console.log("error:", error);
+      setErrorMessage(error.response.data.error);
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 3000);
     }
-  }
+  };
 
-  const removeBlog = async blog => {
-    const conf = window.confirm(`Remove blog ${blog.title} by ${blog.author}`)
+  const removeBlog = async (blog) => {
+    const conf = window.confirm(`Remove blog ${blog.title} by ${blog.author}`);
     //console.log('conf:', conf)
     if (conf) {
       try {
-        await blogService.deleteBlog(blog)
-        setMessage('Blog delete successfully')
-        setTimeout(() => { setMessage('') }, 3000)
-        updateBlogsToShow()
+        await blogService.deleteBlog(blog);
+        setMessage("Blog delete successfully");
+        setTimeout(() => {
+          setMessage("");
+        }, 3000);
+        updateBlogsToShow();
       } catch (error) {
-        console.log('error', error)
+        console.log("error", error);
       }
     }
-  }
+  };
 
   if (user === null) {
     return (
@@ -121,7 +133,7 @@ const App = () => {
         <ErrorMessage msg={errorMessage} />
         <LoginForm login={login} />
       </div>
-    )
+    );
   }
 
   return (
@@ -134,15 +146,20 @@ const App = () => {
         <button onClick={handleLogout}>logout</button>
       </div>
 
-      <ToggleViews buttonLabel='new blog' ref={blogFormRef}>
+      <ToggleViews buttonLabel="new blog" ref={blogFormRef}>
         <NewBlogForm addNewBlog={addNewBlog} />
       </ToggleViews>
 
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} addOneLike={addOneLike} removeBlog={removeBlog} />
-      )}
+      {blogs.map((blog) => (
+        <Blog
+          key={blog.id}
+          blog={blog}
+          addOneLike={addOneLike}
+          removeBlog={removeBlog}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Part5/bloglist-frontend/src/components/Blog.jsx
+++ Part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,48 +1,69 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Blog = ({ blog, addOneLike, removeBlog }) => {
-  const [showDetails, setShowDetails] = useState(false)
+  const [showDetails, setShowDetails] = useState(false);
 
   const showDeleteButtton = () => {
-    const userLoggedIn = window.localStorage.getItem('loggedBlogAppUser')
+    const userLoggedIn = window.localStorage.getItem("loggedBlogAppUser");
     if (userLoggedIn) {
-      const user = JSON.parse(userLoggedIn)
-      return user.username === blog.user.username
+      const user = JSON.parse(userLoggedIn);
+      return user.username === blog.user.username;
     }
-    return false
-  }
+    return false;
+  };
 
   const toggleVisibility = () => {
-    setShowDetails(!showDetails)
-  }
+    setShowDetails(!showDetails);
+  };
 
   const blogStyle = {
     paddingTop: 7,
     paddingLeft: 2,
     paddingBottom: 5,
-    border: 'solid',
+    border: "solid",
     borderWidth: 1,
-    marginBottom: 5
-  }
+    marginBottom: 5,
+  };
 
   const blogDetails = () => {
     return (
-      <div className='div_blogDetails' data-testid='div_blogDetails'>
-        <div className='div_url'>{blog.url}</div>
-        <div className='div_likes'>likes {blog.likes} <button id='btn_addonelike' onClick={() => { addOneLike(blog) }}>like</button></div>
+      <div className="div_blogDetails" data-testid="div_blogDetails">
+        <div className="div_url">{blog.url}</div>
+        <div className="div_likes">
+          likes {blog.likes}{" "}
+          <button
+            id="btn_addonelike"
+            onClick={() => {
+              addOneLike(blog);
+            }}
+          >
+            like
+          </button>
+        </div>
         <div>{blog.user.name}</div>
-        {showDeleteButtton() && <button onClick={() => { removeBlog(blog) }} style={{ background: 'blue' }}>remove</button>}
+        {showDeleteButtton() && (
+          <button
+            onClick={() => {
+              removeBlog(blog);
+            }}
+            style={{ background: "blue" }}
+          >
+            remove
+          </button>
+        )}
       </div>
-    )
-  }
+    );
+  };
 
   return (
-    <div style={blogStyle} data-testid='blog_info_div'>
+    <div style={blogStyle} data-testid="blog_info_div">
       {blog.title} {blog.author}
-      <button id='btn_show_details' onClick={toggleVisibility}>{showDetails ? 'hide' : 'show'}</button>
+      <button id="btn_show_details" onClick={toggleVisibility}>
+        {showDetails ? "hide" : "show"}
+      </button>
       {showDetails && blogDetails()}
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
