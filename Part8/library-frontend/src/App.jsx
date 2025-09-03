9942aa7d243de25272e703b5f8d22071be7e2aa9
Atstincer <atstincer87@gmail.com>
2025-09-03 11:36:57 +0200

    8.17 Listing books

--- Part8/library-frontend/src/App.jsx
+++ Part8/library-frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Books from './components/Books'
 import NewBook from './components/NewBook'
 import { ALL_AUTHORS, ALL_BOOKS } from './querys'
 
+//valid token Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImF0c3RpbmNlciIsImlkIjoiNjhiNTY4YjM2OGE1MzZlZWY1ZmYyMmRjIiwiaWF0IjoxNzU2NzE5NDQxfQ.6hGSXdywDWJ1gQQ2Wug93lfNVZp9CfGE5xzN4shXdkw
+
 const App = () => {
   const [page, setPage] = useState('authors')
   const response_authors = useQuery(ALL_AUTHORS, {
--- Part8/library-frontend/src/components/Books.jsx
+++ Part8/library-frontend/src/components/Books.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 
 const Books = ({ show, response }) => {
-  if (!show) {
+  if (!show || !response.data) {
     return null
   }
 
@@ -22,11 +22,11 @@ const Books = ({ show, response }) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
+          {books.map((b) => (
+            <tr key={b.title}>
+              <td>{b.title}</td>
+              <td>{b.author.name}</td>
+              <td>{b.published}</td>
             </tr>
           ))}
         </tbody>
