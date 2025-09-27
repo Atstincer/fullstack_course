e30617c4ece1233dbcde34f8c15921b3a287950f
Atstincer <atstincer87@gmail.com>
2025-09-27 21:37:59 +0200

    9.3 Command line - body mass index

--- Part9/1-7/bmiCalculator.ts
+++ Part9/1-7/bmiCalculator.ts
@@ -1,15 +1,46 @@
-const calculateBmi = (height: number, weight: number): string => {
-  const heightInM = height / 100;
-  const bmi = weight / (heightInM * heightInM);
-  if (bmi < 18.5) {
-    return "underweight";
-  } else if (bmi >= 18.5 && bmi <= 24.9) {
-    return "normal weight";
-  } else if (bmi >= 25 && bmi <= 29.9) {
-    return "overweight";
-  } else if (bmi >= 30) {
-    return "obese";
+interface Values {
+  height: number;
+  weight: number;
+}
+
+const getValidArgs = (args: string[]): Values => {
+  if (args.length < 4) throw new Error("No enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (isNaN(Number(args[2])) || isNaN(Number(args[3]))) {
+    throw new Error("Arguments are no valid");
+  }
+
+  return {
+    height: Number(args[2]),
+    weight: Number(args[3]),
+  };
+};
+
+const calculateBmi = () => {
+  try {
+    const { height, weight } = getValidArgs(process.argv);
+    console.log("height", height);
+    console.log("weight", weight);
+
+    const heightInM = height / 100;
+    const bmi = weight / (heightInM * heightInM);
+    if (bmi < 18.5) {
+      console.log("underweight");
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+      console.log("normal weight");
+    } else if (bmi >= 25 && bmi <= 29.9) {
+      console.log("overweight");
+    } else if (bmi >= 30) {
+      console.log("obese");
+    }
+  } catch (error: unknown) {
+    let msg: string = "Something went wrong...";
+    if (error instanceof Error) {
+      msg += "Error: " + error.message;
+    }
+    console.log(msg);
   }
 };
 
-console.log(calculateBmi(180, 74));
+calculateBmi();
