34e6065a27b5ca5a0433888e42d02229e5639504
Atstincer <atstincer87@gmail.com>
2025-10-06 09:50:29 +0200

    9.5 WebBMI

--- Part9/1-7/bmiCalculator.ts
+++ Part9/1-7/bmiCalculator.ts
@@ -3,44 +3,70 @@ interface CalculatorValues {
   weight: number;
 }
 
-const getValidArgs = (args: string[]): CalculatorValues => {
+const getValidCMDArgs = (args: string[]): CalculatorValues => {
   if (args.length < 4) throw new Error("No enough arguments");
   if (args.length > 4) throw new Error("Too many arguments");
+  return getValues(args[2], args[3]);
+};
 
-  if (isNaN(Number(args[2])) || isNaN(Number(args[3]))) {
+const getValidStringQueryArgs = (
+  height: string,
+  weight: string
+): CalculatorValues => {
+  if (!height || !weight) throw new Error("No the right arrguments");
+  return getValues(height, weight);
+};
+
+const getValues = (value1: string, value2: string): CalculatorValues => {
+  if (isNaN(Number(value1)) || isNaN(Number(value2))) {
     throw new Error("Arguments are no valid");
   }
 
   return {
-    height: Number(args[2]),
-    weight: Number(args[3]),
+    height: Number(value1),
+    weight: Number(value2),
   };
 };
 
-const calculateBmi = () => {
-  try {
-    const { height, weight } = getValidArgs(process.argv);
-    console.log("height", height);
-    console.log("weight", weight);
+const calculateBmi = ({ height, weight }: CalculatorValues): string => {
+  console.log("height", height);
+  console.log("weight", weight);
 
-    const heightInM = height / 100;
-    const bmi = weight / (heightInM * heightInM);
-    if (bmi < 18.5) {
-      console.log("underweight");
-    } else if (bmi >= 18.5 && bmi <= 24.9) {
-      console.log("normal weight");
-    } else if (bmi >= 25 && bmi <= 29.9) {
-      console.log("overweight");
-    } else if (bmi >= 30) {
-      console.log("obese");
-    }
+  const heightInM = height / 100;
+  const bmi = weight / (heightInM * heightInM);
+  if (bmi < 18.5) {
+    return "underweight";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    return "normal weight";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    return "overweight";
+  } else {
+    //bmi >= 30
+    return "obese";
+  }
+};
+
+const calculateCMDBMI = (): string => {
+  try {
+    const args = getValidCMDArgs(process.argv);
+    return calculateBmi(args);
   } catch (error: unknown) {
     let msg: string = "Something went wrong...";
     if (error instanceof Error) {
       msg += "Error: " + error.message;
     }
-    console.log(msg);
+    return msg;
   }
 };
 
-calculateBmi();
+export const calculateStringQueryBMI = (
+  height: string,
+  weight: string
+): string => {
+  const args = getValidStringQueryArgs(height, weight);
+  return calculateBmi(args);
+};
+
+if (require.main === module) {
+  console.log(calculateCMDBMI());
+}
--- Part9/1-7/index.ts
+++ Part9/1-7/index.ts
@@ -1,8 +1,29 @@
 import express from "express";
+import { calculateStringQueryBMI } from "./bmiCalculator";
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack");
+});
+
+app.get("/bmi", (req, res) => {
+  console.log(req.query);
+  const height = typeof req.query.height === "string" ? req.query.height : "";
+  const weight = typeof req.query.weight === "string" ? req.query.weight : "";
+  try {
+    const bmiResult = calculateStringQueryBMI(height, weight);
+    res.send({
+      weight: 72,
+      height: 180,
+      bmi: bmiResult,
+    });
+  } catch (error: unknown) {
+    let errorMsg: string = "Something went wrong...";
+    if (error instanceof Error) {
+      errorMsg += "Error: " + error.message;
+    }
+    res.send({ error: errorMsg });
+  }
 });
 
 const PORT = 3003;
